2d3393f3565953e4c637eea299797bdd6325e994
Laura <garch.laura@gmail.com>
2023-12-16 23:32:37 +0100

    cpp02

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 16:34:21 by laugarci          #+#    #+#             */
+/*   Updated: 2023/12/14 17:53:43 by laugarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string>
+
+class Fixed {
+	private:
+		int _fixed;
+		static const int _frac = 8;
+	public:
+		Fixed();
+		Fixed(const int value);
+		Fixed(const float number)
+		~Fixed();
+		Fixed(const Fixed& oldfix);
+		Fixed&	operator=(const Fixed& oldfix);
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		floeat toFloat(void) const;
+		int	toInt(void) const;
+};
+
+#endif
