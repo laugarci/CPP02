2be38d59b75f814de4c19773f881810a6af4547b
Laura Garcia Arch <garch.laura@gmail.com>
2023-12-19 12:23:26 +0100

    cpp02

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 16:01:02 by laugarci          #+#    #+#             */
+/*   Updated: 2023/12/19 12:12:14 by laugarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_fixed = 0;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const int num)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_fixed = num << _frac; 
+}
+
+Fixed::Fixed(const Fixed& oldfix)
+{
+	*this = oldfix;
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& oldfix)
+{
+	if (this != &oldfix)
+		this->_fixed = oldfix.getRawBits();
+	std::cout << "Copy assigment operator called" << std::endl;
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	return (this->_fixed);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member called" << std::endl;
+	this->_fixed = raw;
+}
+
+Fixed::Fixed(const float num)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_fixed = roundf(num * (1 << _frac));
+}
+
+float	Fixed::toFloat() const
+{
+	return (this->_fixed / (float)(1 << _frac));
+}
+
+int		Fixed::toInt(void) const
+{
+	return (this->_fixed >> _frac);
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed)
+{
+	out << fixed.toFloat();
+	return (out);
+}
---
+++ ex02/Fixed.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 16:34:21 by laugarci          #+#    #+#             */
+/*   Updated: 2023/12/19 12:23:14 by laugarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string>
+
+class Fixed {
+	private:
+		int _fixed;
+		static const int _frac = 8;
+	public:
+		Fixed();
+		Fixed(const int num);
+		Fixed(const float num);
+		~Fixed();
+		Fixed(const Fixed& oldfix);
+		Fixed& operator=(const Fixed& oldfix);
+		Fixed& operator>(const Fixed& oldfix);
+		Fixed& operator<(const Fixed& oldfix);
+		Fixed& operator>=(const Fixed& oldfix);
+		Fixed& operator==(const Fixed& oldfix);
+		Fixed& operator!=(const Fixed& oldfix);
+		Fixed& operator+(const Fixed& oldfix);
+		Fixed& operator-(const Fixed& oldfix);
+		Fixed& operator*(const Fixed& oldfix);
+		Fixed& operator/(const Fixed& oldfix);
+		//faltan
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat (void) const;
+		int toInt (void) const;
+};
+
+std::ostream&	operator<<( std::ostream& out, const Fixed& fixed );
+
+#endif
