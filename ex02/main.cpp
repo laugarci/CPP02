a86970e2a5e3a8f7ae6cddd7b4fb742a710da189
Laura Garcia Arch <garch.laura@gmail.com>
2024-01-24 11:14:35 +0100

    cpp02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 11:53:17 by laugarci          #+#    #+#             */
-/*   Updated: 2024/01/12 12:26:12 by laugarci         ###   ########.fr       */
+/*   Updated: 2024/01/24 11:14:22 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 int main( void )
 {
+	std::cout << "[ SUBJECT TESTS ]" << std::endl;
+	
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed c (60);
+	Fixed		d( 10.3f );
+	const Fixed	e( 7.3f );
 	
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
@@ -25,16 +30,42 @@ int main( void )
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
 
-	std::cout << std::endl << "MORE TESTS" << std::endl;
-	Fixed c (60);
+
+	std::cout << std::endl << "[ POST/PRE INCREMENT TESTS ]" << std::endl;	
+	std::cout << "a: " << a << std::endl;
+	std::cout << "++a: " << ++a << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "a--: " << a-- << std::endl;
+	std::cout << "a++: " << a++ << std::endl;
+	std::cout << "a--: " << a-- << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "--a: " << --a << std::endl;
+	std::cout << "a: " << a << std::endl;
+
+	std::cout << std::endl << "[ MAX/MIN TESTS ]" << std::endl;
+	std::cout << "max( a, b): " << Fixed::max( a, b ) << std::endl;
+	std::cout << "min( a, b ): " << Fixed::min( a, b ) << std::endl;
+
+	std::cout << std::endl << "[ ARITHMETIC OPERATORS ]" << std::endl;
+	c = 60;
 	a = 20;
-	c = 900;
 	std::cout << "a + b = " << a+b << std::endl;
 	std::cout << "b + c = " << b+c << std::endl;
 	std::cout << "c / b = " << c/b << std::endl;
 	std::cout << "c / a = " << c/a << std::endl;
 	std::cout << "a * b = " << a*b << std::endl;
-	std::cout << Fixed::min(a, c) << std::endl;
+
+	std::cout << std::endl << "[ COMPARISON OPERATORS ]" << std::endl;
+	std::cout << "a > b: " << ( a > b ) << std::endl;
+	std::cout << "a >= b: " << ( a >= b ) << std::endl;
+	std::cout << "a < b: " << ( a < b ) << std::endl;
+	std::cout << "a <= b: " << ( a <= b ) << std::endl;
+	std::cout << "c > d: " << ( c > d ) << std::endl;
+	std::cout << "c >= d: " << ( c >= d ) << std::endl;
+	std::cout << "c < d: " << ( c < d ) << std::endl;
+	std::cout << "c <= d: " << ( c <= d ) << std::endl;
+	std::cout << "c == d: " << ( c == d ) << std::endl;
+	std::cout << "c != d: " << ( c != d ) << std::endl;
 
 	return 0;
 }
